f1ebe1818397a9abce088b23950e07aeb20aa5ff
Andrew Moon <andrewjm80@gmail.com>
2019-10-05 12:30:35 -0500

    update2

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -3,7 +3,7 @@ import { authDomain, authClientId } from '../constants';
 export const AUTH_CONFIG = {
   domain: authDomain,
   clientId: authClientId,
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'https://amsmovieraterapp.netlify.com/callback'
 };
 //https://amsrecordcollection.netlify.com/callback
 //http://localhost:3000/callback
